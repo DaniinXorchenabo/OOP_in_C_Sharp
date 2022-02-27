dfad2fe389f1b53ab37ab9d70a56c025a0cc978d
DaniinXorchenabo <rkbcu@mail.ru>
2022-02-27 18:03:33 +0300

    create first lab

--- lab_1/Program.cs
+++ lab_1/Program.cs
@@ -1,3 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Dynamic;
+using System.Net.Sockets;
+using lab_1;
+
+public class Program
+{
+    static int Main()
+    {
+        Console.WriteLine("Добрый день, это лабораторная работа № 1 группы 20ВП1, реализованная бригадой в составе: Дьячкова Д.А.");
+        Console.WriteLine("It is first lab");
+        var obj = new TelephoneStation(address:"Велосипедная, 32е", companyName:"Тестовая компания № 1", countOfUsers:8);
+        Console.WriteLine(obj.ToString());
+        Console.WriteLine($"Адрес компании: {obj.Address ?? "Не установлен"}");
+        obj.GetCompanyName();
+        Console.WriteLine($"Число клиентов компании в шестнадцатиричной системе счисления: { (obj.CountOfUsers is null ? "<unset>": Convert.ToString((int) obj.CountOfUsers, 8))}");
+        List<string> allFields;
+        Console.WriteLine(
+            "Далее можно изменять значения полей класа. Для этого введите одно из следующих полей и через пробел его новое значение");
+        Console.WriteLine($"Возможные поля: {string.Join(", ", (allFields = obj.GetAllFields()))}");
+        while (true)
+        {
+            string inputData = Console.ReadLine() ?? "";
+            string[] raw_data;
+            if ((raw_data = inputData.Split(" ")).Length > 1 && obj.SetSomeValue(raw_data[0],
+                    string.Join(" ", raw_data.Where((x, index) => index != 0))))
+            {
+                Console.WriteLine(obj.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Некорректное название поля или некорректное значение поля");
+            }
+        }
+        return 0;
+    }
+}
