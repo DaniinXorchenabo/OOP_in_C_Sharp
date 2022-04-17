733e973cc710a0031694981e6615be1f876251ec
DaniinXorchenabo <rkbcu@mail.ru>
2022-04-17 17:52:53 +0300

    created 8th laboratory work files.

---
+++ lab_8/src/PhoneStationDict.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace lab_8;
+
+public class PhoneStationDict<TKey, TValue>:  ICollection<KeyValuePair<TKey,TValue>>
+{
+    protected Dictionary<TKey, TValue> Container = new Dictionary<TKey, TValue>();
+    public int Count { get; }
+    public bool IsReadOnly { get; }
+
+    public void Add(KeyValuePair<TKey, TValue> f)
+    {
+        throw new System.NotImplementedException();
+    }
+    
+    public void Clear()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool Contains(KeyValuePair<TKey, TValue> item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool Remove(KeyValuePair<TKey, TValue> item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ lab_8/src/stations/AbstractAts.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+namespace lab_8;
+
+
+
+public abstract class AbstractAts : IDisposable
+{
+    private static IEnumerable<PropertyInfo> _PublicProperties;
+
+    protected virtual IEnumerable<PropertyInfo> PublicProperties
+    {
+        get => AbstractAts._PublicProperties;
+        set => AbstractAts._PublicProperties = value;
+    }
+
+    public static int ObjectCounter { get; private set; } = 0;
+    protected bool Disposed = false;
+    private static List<AbstractAts> _AllTelephoneStations { get; set; } = new List<AbstractAts>() { };
+    public static List<AbstractAts> _AllObjects { get; set; } = new List<AbstractAts>() { };
+
+    protected virtual List<AbstractAts> AllObjects
+    {
+        get => _AllObjects;
+        set => _AllObjects = value;
+    }
+
+    public static event Action<AbstractAts> AddItemEvent;
+    public static event Action<AbstractAts> RemoveItemEvent;
+
+    static AbstractAts()
+    {
+        _PublicProperties = typeof(AbstractAts)
+            .GetProperties()
+            .Where(x => x.GetMethod != null && x.GetMethod.IsPublic && !x.GetMethod.IsStatic);
+    }
+
+    /// <summary> Адресс компании</summary>
+    public string? Address { get; set; } = null;
+
+    /// <summary> Количество клиентов компании</summary>
+    public int? CountOfUsers { get; set; } = null;
+
+    /// <summary> стоимость ежемесячных услуг</summary>
+    public Decimal? MonthlySubscriptionFee { get; set; } = null;
+
+    /// <summary> Название компании</summary>
+    public string? CompanyName { get; set; } = null;
+
+    /// <summary> ИНН компании</summary>
+    public string? Inn { get; set; } = null;
+
+    /// <summary> Индекс доверия пользователей этой компании</summary>
+    public float? TrustIndex { get; set; } = null;
+
+    /// <summary> Дата основания компании</summary>
+    public string? DateOfFoundation { get; set; } = null;
+
+    public AbstractAts()
+    {
+        ObjectCounter++;
+        AbstractAts._AllTelephoneStations += this;
+    }
+
+    public AbstractAts(Random random)
+    {
+
+       
+        foreach (var field in PublicProperties)
+        {
+            if (field.PropertyType == typeof(string))
+            {
+                SetSomeValue(field.Name, GenerateChar(
+                    Convert.ToInt32(Math.Floor(20 * random.NextDouble() + 1)),
+                    random,
+                    65,
+                    122
+                ));
+            }
+            else if (field.PropertyType == typeof(int) || field.PropertyType == typeof(int?))
+            {
+                SetSomeValue(field.Name, Convert.ToInt32(Math.Floor(100000 * random.NextDouble())));
+            }
+            else if (field.PropertyType == typeof(float) || field.PropertyType == typeof(float?))
+            {
+                SetSomeValue(field.Name, ((100000 * random.NextDouble())));
+            }
+            else if (field.PropertyType == typeof(double) || field.PropertyType == typeof(double?))
+            {
+                SetSomeValue(field.Name, Convert.ToDouble((100000 * random.NextDouble())));
+            }
+            else if (field.PropertyType == typeof(Decimal) || field.PropertyType == typeof(Decimal?))
+            {
+                SetSomeValue(field.Name, Convert.ToDecimal((1000 * random.NextDouble())));
+            }
+        }
+        AbstractAts._AllTelephoneStations += this;
+        ObjectCounter++;
+    }
+
+    public string GenerateChar(Random random, int minInterval = 48, int maxInterval = 58)
+    {
+        return Convert
+            .ToChar(Convert.ToInt32(Math.Floor((maxInterval - minInterval) * random.NextDouble() + minInterval)))
+            .ToString();
+    }
+
+    public string GenerateChar(int count, Random random, int minInterval = 48, int maxInterval = 58)
+    {
+        string randomString = "";
+
+        for (int i = 0; i < count; i++)
+        {
+            randomString += GenerateChar(random, minInterval, maxInterval);
+        }
+
+        return randomString;
+    }
+
+    /// <summary> переопределение для печати объекта</summary>
+    public override string ToString()
+    {
+        return $"{CompanyName ?? "<unknown>"}";
+    }
+
+    /// <summary> Получить полный</summary>
+    public string ToLongString()
+    {
+        return $"ATC=>[{string.Join(", ", ParamsAsStrings())}]";
+    }
+
+
+    /// <summary> Получить пары "название параметра - значение" в виде строки</summary>
+    public IEnumerable<string> ParamsAsStrings()
+    {
+        object? a;
+        return PublicProperties.Select(
+            x => $"{x.Name}={((a = x.GetValue(this)) == null ? "<unset>" : $"\"{a.ToString()}\"")}");
+    }
+
+    /// <summary> Напечатать название компании</summary>
+    public void GetCompanyName()
+    {
+        Console.WriteLine($"А название этой компании - {CompanyName ?? "<Сокрыто мировым правительством рептилий>"}");
+    }
+
+    /// <summary> получить все поля объекта</summary>
+    public List<string> GetAllFields()
+    {
+        return PublicProperties.Select(x => x.Name).ToList();
+    }
+
+    /// <summary> установить значение любого поля по его имени</summary>
+    public bool SetSomeValue(string targetFieldName, object newFieldValue)
+    {
+        bool status = false;
+        PropertyInfo? findField;
+        if ((findField = PublicProperties.FirstOrDefault(i => i.Name == targetFieldName)) is not null)
+        {
+            Type t = Nullable.GetUnderlyingType(findField.PropertyType) ?? findField.PropertyType;
+            object safeValue = (newFieldValue == null) ? null : Convert.ChangeType(newFieldValue, t);
+            findField.SetValue(this, safeValue, null);
+            status = true;
+        }
+
+        return status;
+    }
+
+    /// <summary> получить значения любого поля из объекта </summary>
+    public object GetSomeValue(string targetFieldName)
+    {
+        PropertyInfo? findField;
+        if ((findField = PublicProperties.FirstOrDefault(i => i.Name == targetFieldName)) is not null)
+        {
+            // ((a = x.GetValue(this)) == null ? "<unset>" : $"\"{a.ToString()}\"")}")
+            return findField.GetValue(this);
+        }
+
+        return null;
+    }
+
+
+    /// <summary> Деструктор</summary>
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary> Деструктор</summary>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (Disposed) return;
+        if (disposing)
+        {
+            // Освобождаем управляемые ресурсы
+        }
+
+        // освобождаем неуправляемые объекты
+        Disposed = true;
+        ObjectCounter--;
+        _AllTelephoneStations -= this;
+        AllObjects -= this;
+    }
+
+    /// <summary> Деструктор</summary>
+    ~AbstractAts()
+    {
+        Dispose(false);
+    }
+
+    public static List<AbstractAts> operator +(List<AbstractAts> list, AbstractAts station)
+    {
+        list.Add(station);
+        if (list == _AllTelephoneStations)
+        {
+            AddItemEvent(station);
+        }
+        return list;
+    }
+    public static List<AbstractAts> operator -(List<AbstractAts> list, AbstractAts station)
+    {
+        list.Remove(station);
+        if (list == _AllTelephoneStations)
+        {
+            RemoveItemEvent(station);
+        }
+        return list;
+    }
+
+    public abstract void CreateCustomizedName();
+
+}
