301042e035ee0ee619fb37cb73d843a9fc8d81af
DaniinXorchenabo <rkbcu@mail.ru>
2022-04-18 14:57:57 +0300

    небльшие косметические обновления конструкторов, деструкторов, индексаторов и т.д. в PhoneStationDict<TKey, TValue>.

--- lab_8/src/PhoneStationDict.cs
+++ lab_8/src/PhoneStationDict.cs
@@ -6,17 +6,47 @@ using System.Windows.Forms;
 
 namespace lab_8;
 
-public class PhoneStationDict<TKey, TValue>:  ICollection<KeyValuePair<TKey,TValue>> where TValue: AbstractAts
+public class PhoneStationDict<TKey, TValue> : ICollection<KeyValuePair<TKey, TValue>> where TValue : AbstractAts, IDisposable
 {
     protected Dictionary<TKey, TValue> Container = new Dictionary<TKey, TValue>();
-    public int Count { get; }
+    private bool _disposed = false;
+    public int Count
+    {
+        get => Container.Count;
+    }
+
     public bool IsReadOnly { get; }
+
+    public PhoneStationDict()
+    {
+        Container = new Dictionary<TKey, TValue>();
+    }
+
+    public PhoneStationDict(string keyParamName, params TValue[] data)
+    {
+        Container = new Dictionary<TKey, TValue>();
+        var getter = typeof(TValue)
+            .GetProperty(keyParamName);
+        var keyValuePairs = data.Select(x =>
+        {
+            var typedKey = getter != null && getter.GetValue(x) is TKey ? (TKey) getter.GetValue(x) : default;
+            var t = new KeyValuePair<TKey, TValue>(typedKey, x);
+            this.Add(t);
+            return t;
+        });
+    }
+
+    public TValue this[TKey index]
+    {
+        get => Container[index];
+        set => Container[index] = value;
+    }
 
     public void Add(KeyValuePair<TKey, TValue> keyVal)
     {
         Container.Add(keyVal.Key, keyVal.Value);
     }
-    
+
     public void Clear()
     {
         Container.Clear();
@@ -44,7 +74,7 @@ public class PhoneStationDict<TKey, TValue>:  ICollection<KeyValuePair<TKey,TVal
             throw new ArgumentException();
         }
 
-        foreach(var item in Container.Select((item, ind) => (item: item, ind: ind + arrayIndex)))
+        foreach (var item in Container.Select((item, ind) => (item: item, ind: ind + arrayIndex)))
         {
             array[item.ind] = item.item;
         }
@@ -63,5 +93,31 @@ public class PhoneStationDict<TKey, TValue>:  ICollection<KeyValuePair<TKey,TVal
     IEnumerator IEnumerable.GetEnumerator()
     {
         return GetEnumerator();
+    }
+    
+    /// <summary> Деструктор</summary>
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary> Деструктор</summary>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed) return;
+        if (disposing)
+        {
+            // Освобождаем управляемые ресурсы
+        }
+
+        // освобождаем неуправляемые объекты
+        _disposed = true;
+    }
+
+    /// <summary> Деструктор</summary>
+    ~PhoneStationDict()
+    {
+        Dispose(false);
     }
 }
